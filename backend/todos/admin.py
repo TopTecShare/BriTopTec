69419289cceed0b2ab480a6dfc77c714fafa384f
TopTecShare <eestimorol.ice9208g@gmail.com>
2021-09-21 17:09:45 +0900

    OOps, you implement TodoAdmin

--- backend/todos/admin.py
+++ backend/todos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Todo
 
-# Register your models here.
+class TodoAdmin(admin.ModelAdmin):
+  list = ('title', 'description', 'completed')
+
+admin.site.register(Todo, TodoAdmin)
--- backend/todos/models.py
+++ backend/todos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+   title = models.CharField(max_length=100)
+   description = models.TextField()
+   completed = models.BooleanField(default=False)
+
+   def _str_(self):
+     return self.title
